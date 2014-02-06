b09a010ddddf3bc84a6c61d879a32a7a538a7291
cclaerhout <cedric.claerhout@free.fr>
2014-02-06 18:03:41 +0800

    Version 1.1 released
    Compatible with the last version of TinyMCE Quattro (fast reload & menu)

--- upload/library/Sedo/FaBbCode/Listeners.php
+++ upload/library/Sedo/FaBbCode/Listeners.php
@@ -8,4 +8,16 @@ class Sedo_FaBbCode_Listeners
 			$extend[] = 'Sedo_FaBbCode_ControllerPublic_Editor';
 		}
 	}
+
+	public static function mceConfig($mceConfigObj)
+	{
+		if($mceConfigObj->hasMenu('adv_insert'))
+		{
+			$mceConfigObj->addMenuItem('bbm_sedo_adv_fa', 'adv_insert', '@adv_insert_3');
+		}
+		else
+		{
+			$mceConfigObj->addMenuItem('bbm_sedo_adv_fa', 'insert', '@insert_2');
+		}
+	}	
 }
